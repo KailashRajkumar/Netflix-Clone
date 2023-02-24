b109dbd6114c468a38bd9471b7ec45716906a0f9
Kailash Rajkumar <93399794+KailashRajkumar@users.noreply.github.com>
2023-02-24 12:14:00 +0530

    Add files via upload

---
+++ netflix-ui/src/utils/constants.js
@@ -0,0 +1,2 @@
+export const API_KEY = "0b693c00e4e865bc2746caa6fff781dd";
+export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
---
+++ netflix-ui/src/utils/firebase-config.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyBzR21p5SfL-2YliiK2RniGRrAuIGZhnF0",
+  authDomain: "react-netflix-clone-e201c.firebaseapp.com",
+  projectId: "react-netflix-clone-e201c",
+  storageBucket: "react-netflix-clone-e201c.appspot.com",
+  messagingSenderId: "957196377424",
+  appId: "1:957196377424:web:c528e613d2d74b4f762055",
+  measurementId: "G-GS3DWMGPZT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const firebaseAuth = getAuth(app);
